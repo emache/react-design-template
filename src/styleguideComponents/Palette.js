d4bb7b55dc6090217838b580fc6e7fc03855eaa9
Marie Lu Vinh <mluvinh@gmail.com>
2017-11-02 22:35:19 +0000

    Experimenting with styled components

--- src/styleguideComponents/Palette.js
+++ src/styleguideComponents/Palette.js
@@ -2,26 +2,27 @@
 import React from "react";
 import { View } from "react-sketchapp";
 import Swatch from "./Swatch";
+import styled from "styled-components/primitives";
 
 type P = {
     name: string
 };
 
-const Palette = ({ color }: P) => (
-    <View
-        name="colour-palette"
-        style={{
-            width: "1200",
-            flexWrap: "wrap",
-            flexDirection: "row",
-            paddingLeft: 0,
-            paddingRight: 0
-        }}
-    >
+const Palette = styled.View`
+    width: 1200;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 0;
+`;
+
+export default ({ color }: P) => (
+    <Palette name="Palette">
         {Object.keys(color).map(name => (
             <Swatch color={color[name]} name={name} key={name} />
         ))}
-    </View>
+    </Palette>
 );
 
-export default Palette;
+// export default Palette;
